1b4f9b0ef95bf62f808fd9b2160cf5deba130bb1
Daniel Vitacolonna <44033488+VitaDani@users.noreply.github.com>
2024-08-04 22:22:31 -0600

    Create the darkening effect for the site
    Make the squares turn black as the mouse hovers over them, and create the button allowing the user to choose a grid size.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,38 @@
+/* Setting the variables for the flex container and what will be used to determine
+   the grid squares' width and height */
 let container = document.querySelector(".container");
-let row_column = 16;
-let square_width = container.computedStyleMap().get('width')['value'] / row_column;
-let square_height = container.computedStyleMap().get('height')['value'] / row_column;
+// let row_column = 16;
 
-for (let i = 0; i < row_column; i++) {
-    for (let j = 0; j < row_column; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.style.width = `${square_width}px`;
-        square.style.height = `${square_height}px`;
-        container.appendChild(square);
+// Create the function that makes the grid of div squares
+function createGrid(row_column = 16) {
+    let square_width = container.computedStyleMap().get('width')['value'] / row_column;
+    let square_height = container.computedStyleMap().get('height')['value'] / row_column;
+    
+    for (let i = 0; i < row_column; i++) {
+        for (let j = 0; j < row_column; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.style.width = `${square_width}px`;
+            square.style.height = `${square_height}px`;
+            container.appendChild(square);
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = "black";
+            });
+        }
     }
 }
-console.log(square_width);
+
+// Make the edit button clickable and asks for input
+let grid_size = document.querySelector("#grid_size");
+
+grid_size.addEventListener("click", () => {
+    let user_input = prompt("What size would you like your grid? (1 - 100)");
+    if (user_input > 100 || user_input < 1) {
+        alert("The number you have chosen is not within parameters");
+        exit;
+    }
+    container.replaceChildren();
+    createGrid(user_input);
+});
+
+createGrid();
